6fe52c05369fa1a62bb9149073d65a1329cf3879
chualec <63263128+chualec@users.noreply.github.com>
2023-01-25 10:32:23 -0500

    Moved things into folders

---
+++ HapChIP/HapChIP.py
@@ -0,0 +1,11 @@
+import a
+import b
+
+def read_in_data(path_to_data):
+    filename = path_to_data
+    df = data.frame(path_to_data)
+    df = data_treatment_1(df)
+
+def data_treatment_1(df):
+    #some code here to read in and perform QC on the data.
+    
--- hichip/hichip.py
+++
@@ -1,9 +0,0 @@
-import a
-import b
-
-def func_1():
-    pass
-
-def func_2():
-    pass
-    
