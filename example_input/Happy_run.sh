27bc9e19bb596bdfcbaa44e3fefaac6918526521
chualec <63263128+chualec@users.noreply.github.com>
2023-03-25 18:59:42 -0400

    Create Happy_run.sh

---
+++ example_input/Happy_run.sh
@@ -0,0 +1,6 @@
+python3 ../HapChIP/HapChIP.py -b example.bam -v example.vcf.gz -o ../example_output
+
+samtools index ../example_output/example.bam.hap0.bam
+samtools index ../example_output/example.bam.hap1.bam
+samtools index ../example_output/example.bam.hap2.bam
+samtools index ../example_output/example.bam.hap3.bam
